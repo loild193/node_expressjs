3f0ecacb8196fde5b63c15e648d85fb35d2f7047
leducloi <dekaito193@gmail.com>
2020-04-12 16:41:16 +0700

    done for lession 13 for middleware

--- 1-helloworld/routes/user.route.js
+++ 1-helloworld/routes/user.route.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 var controller = require('../controllers/user.controller');
+var validation = require('../validation/user.validation');
 
 router.get('/', controller.index);
 
@@ -10,7 +11,7 @@ router.get('/search', controller.search);
 
 router.get('/create', controller.getCreate);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validation.postCreate, controller.postCreate);
 
 router.get('/:id', controller.getInfo);
 
